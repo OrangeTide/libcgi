ea658ede3eae0c6da08ee0b0449318a2f6040164
Jon Mayo <jon.mayo@gmail.com>
2009-03-04 13:32:31 -0800

    keygen() function accepts a character set parameter.

--- keygen.c
+++ keygen.c
@@ -2,17 +2,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "keygen.h"
 
 /* generates a random string between lower and upper characters */
-char *random_string(unsigned lower, unsigned upper) {
+char *random_string(unsigned lower, unsigned upper, const char *charset) {
         char *ret;
         unsigned i, len;
+		size_t charset_len;
 		if(upper>lower) {
 			len=rand()%(upper-lower)+lower;
 		} else {
 			/* refuse to do modulus/remainder on zero or negative numbers */
 			len=lower;
+		}
+		if(!charset) {
+			charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+		}
+		charset_len=strlen(charset);
+		if(charset_len<=0) {
+			return NULL; /* failure */
 		}
         ret=malloc(len+1);
         if(!ret) { /* verify the allocation */
@@ -20,7 +29,7 @@ char *random_string(unsigned lower, unsigned upper) {
                 return 0;
         }
         for(i=0;i<len;i++) {
-                ret[i]=(rand()%95)+' ';
+                ret[i]=charset[rand()%charset_len];
         }
         ret[i]=0;
         return ret;
--- keygen.h
+++ keygen.h
@@ -1,5 +1,5 @@
 /* keygen.h */
 #ifndef KEYGEN_H
 #define KEYGEN_H
-char *random_string(unsigned lower, unsigned upper);
+char *random_string(unsigned lower, unsigned upper, const char *charset);
 #endif
